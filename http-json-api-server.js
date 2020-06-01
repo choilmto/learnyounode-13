eb9121b854e0ce811c7f5084682f8ddfbe78ce51
choilm <choilmto@gmail.com>
2020-06-01 19:02:35 -0400

    Complete exercise

--- http-json-api-server.js
+++ http-json-api-server.js
@@ -1,1 +1,30 @@
-// ba kitchen
+var http = require('http');
+var url = require('url');
+function callback(req, res) {
+    if (req.method === 'GET') {
+        var _a = new URL(req.url, "http://" + req.headers.host), pathname = _a.pathname, searchParams = _a.searchParams;
+        var date = new Date(searchParams.get('iso'));
+        var formatted_date = void 0;
+        switch (pathname) {
+            case '/api/parsetime':
+                formatted_date = {
+                    "hour": date.getHours(),
+                    "minute": date.getMinutes(),
+                    "second": date.getSeconds()
+                };
+                break;
+            case '/api/unixtime':
+                formatted_date = {
+                    "unixtime": date.getTime()
+                };
+                break;
+            default:
+                return res.end();
+        }
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify(formatted_date));
+        res.end();
+    }
+}
+var server = http.createServer(callback);
+server.listen(Number(process.argv[2]));
--- http-json-api-server.ts
+++ http-json-api-server.ts
@@ -1,1 +1,40 @@
-// ba kitchen
+const http = require('http');
+const url = require('url');
+
+function callback (req: any, res: any): void {
+    if (req.method === 'GET') {
+        const { pathname, searchParams } = new URL(req.url, `http://${req.headers.host}`);
+        const date: Date = new Date(searchParams.get('iso') as string);
+        type unixtime = {
+            "hour": number,
+            "minute": number,
+            "second": number
+        };
+        type parsetime = {
+            "unixtime": number
+        };
+        let formatted_date: unixtime | parsetime;
+        switch (pathname) {
+            case '/api/parsetime':
+                formatted_date = {
+                    "hour": date.getHours(),
+                    "minute": date.getMinutes(),
+                    "second": date.getSeconds()
+                };
+                break;
+            case '/api/unixtime':
+                formatted_date = {
+                    "unixtime": date.getTime()
+                };
+                break;
+            default:
+                return res.end();
+        }
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify(formatted_date));
+        res.end();
+    }
+}
+
+const server = http.createServer(callback);
+server.listen(Number(process.argv[2]));
